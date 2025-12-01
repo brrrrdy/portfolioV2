b4dd26058a6a15d29ef6195b1c1febc28c988420
Tom Alvarez <127635396+brrrrdy@users.noreply.github.com>
2025-12-01 16:23:37 +0100

    init

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,11 @@ import "./index.css";
 
 function App() {
   return (
-    <div className="min-h-screen flex flex-col">
+    <div className="min-h-screen flex flex-col bg-blue-900 text-white">
       <div id="top" className="h-px w-full relative" />
 
-      {}
       <div className="flex-1 flex items-center justify-center">
-        <h1 className="text-4xl font-bold">Portfolio Coming Soon</h1>
+        <h1 className="text-4xl font-bold text-white">Portfolio Coming Soon</h1>
       </div>
     </div>
   );
