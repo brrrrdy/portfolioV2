327e8d5e63530f5ea1aaff937a2f05a047562b1d
Tom Alvarez <127635396+brrrrdy@users.noreply.github.com>
2025-12-08 12:36:15 +0100

    separate concerns css

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,11 @@
+import heroContent from "../content/hero.json";
+
 function Hero() {
   return (
     <div className="hero-section">
       <div className="main-content">
-        <h1 className="title">tom alvarez</h1>
+        <h1 className="title">{heroContent.headerTitle}</h1>
+        <p className="subtitle">{heroContent.headerSubtitleEn}</p>
       </div>
     </div>
   );
