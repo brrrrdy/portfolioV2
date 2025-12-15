a17ab5ea562a7a5d88c5459346e5c18798156939
Tom Alvarez <127635396+brrrrdy@users.noreply.github.com>
2025-12-15 08:40:52 +0100

    style adjustments

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,59 @@
+import githubLogo from "../assets/images/github-original.svg";
+import linkedinLogo from "../assets/images/linkedin.svg";
+
 function Footer() {
   return (
     <footer className="footer">
       <div className="footer-container">
         <div className="footer-content">
-          <p>&copy; 2025 Tom Alvarez. All rights reserved.</p>
+          <div className="footer-left">
+            <div className="footer-github">
+              <img src={githubLogo} alt="GitHub" className="github-logo" />
+              <p>check out the repo for this project!</p>
+            </div>
+            <p>&copy; 2025 Tom Alvarez. All rights reserved.</p>
+          </div>
+          <div className="footer-sitemap-container">
+            <div className="footer-sitemap">
+              <ul>
+                <li>
+                  <a href="#home">Home</a>
+                </li>
+                <li>
+                  <a href="#projects">Projects</a>
+                </li>
+                <li>
+                  <a href="#toolkit">Toolkit</a>
+                </li>
+                <li>
+                  <a href="#contact">Contact</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="footer-social">
+            <h4>Connect</h4>
+            <div className="social-links">
+              <a
+                href="https://www.linkedin.com/in/tom-alvarez-ryan-80423951/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img
+                  src={linkedinLogo}
+                  alt="LinkedIn"
+                  className="social-logo"
+                />
+              </a>
+              <a
+                href="https://github.com/brrrrdy"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img src={githubLogo} alt="GitHub" className="social-logo" />
+              </a>
+            </div>
+          </div>
         </div>
       </div>
     </footer>
